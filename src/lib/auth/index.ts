3bcf3db4871ab1bcc69bca0fb6f92094961b78fd
shinyj0 <yejins0605@gmail.com>
2026-03-03 17:31:06 +0900

    feat: better-auth 적용

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from "better-auth";
+import { db } from "@/lib/db/client";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as authSchema from "@/lib/db/auth-schema";
+
+export const auth = betterAuth({
+  baseURL:
+    process.env.BETTER_AUTH_URL ??
+    (process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000"),
+  basePath: "/api/auth",
+  trustedOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : []),
+  ],
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...authSchema,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
