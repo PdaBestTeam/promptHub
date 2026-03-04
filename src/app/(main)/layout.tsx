15d3eb7eafb083256d8a7e3a04261532ba9e2d55
shinyj0 <yejins0605@gmail.com>
2026-03-04 20:42:23 +0900

    feat: 사용 안내 모달 구현

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,12 @@
 import AppHeader from "@/components/layouts/app-header";
+import OnboardingModal from "@/components/ui/OnboardingModal";
 import type { ReactNode } from "react";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <AppHeader />
+      <OnboardingModal />
       {children}
     </div>
   );
